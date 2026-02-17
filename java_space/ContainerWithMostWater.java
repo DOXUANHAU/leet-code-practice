33dfcf167e3c6e195c3f08ed74c163241aae29b4
DOXUANHAU <doxuanhau08022003@gmail.com>
2026-02-17 09:25:15 +0700

    module{logic} : fix container using two pointer

--- java_space/ContainerWithMostWater.java
+++ java_space/ContainerWithMostWater.java
@@ -4,19 +4,33 @@ public class ContainerWithMostWater {
     }
 
     
+    // public int maxArea(int[] height){
+        // if(height.length == 0) return 0;
+        // int maxResult = 0;
+        // for (int i = 0; i < height.length; i++) {
+            // for (int j = i+1; j < height.length; j++) {
+                // take all infor of two column 
+                // int temp = height[i] >= height[j] ? height[j] * (j - i) : height[i] * (j - i);
+                // if (maxResult <= temp) {
+                    // maxResult = temp;
+                // }
+            // }
+        // }
+        // return maxResult;
+    // }
+    // 
     public int maxArea(int[] height){
         if(height.length == 0) return 0;
-        int maxResult = 0;
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i+1; j < height.length; j++) {
-                // take all infor of two column 
-                int temp = height[i] >= height[j] ? height[j] * (j -i) : height[i] * (j - i);
-                if (maxResult <= temp) {
-                    maxResult = temp;
-                }
-            }
+        int i =0,j = height.length -1 , result = 0;
+        while(i <= j){
+        int area = height[i] >= height[j] ? height[j] * (j - i) : height[i] * ( j -i);
+        if(result <= area) result = area;
+        if(height[i] >= height[j]) {
+            j--;
+        }else{i++;
         }
-        return maxResult;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
