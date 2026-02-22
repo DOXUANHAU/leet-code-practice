9ee0bbaaddaed76b3f9db4660a01d4d9407e0efb
DOXUANHAU <doxuanhau08022003@gmail.com>
2026-02-22 21:19:51 +0700

    module{logic} : optimize code using switch case in order to use Map

--- java_space/RomanToInteger.java
+++ java_space/RomanToInteger.java
@@ -2,23 +2,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RomanToInteger {
-
-    
-      public static int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
+  public static int romanToInt(String s) {
         int total = 0;
         int prevValue = 0;
 
         for (int i = s.length() - 1; i >= 0; i--) {
-            int currentValue = map.get(s.charAt(i));
+            int currentValue = getValue(s.charAt(i));
 
             if (currentValue < prevValue) {
                 total -= currentValue;
@@ -30,6 +19,19 @@ public class RomanToInteger {
         }
 
         return total;
+    }
+
+    private static int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 
 }
