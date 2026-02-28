cbd43588c8113bde87df795e74dbb5840a48cf35
DOXUANHAU <doxuanhau08022003@gmail.com>
2026-02-28 12:07:52 +0700

    refactor(logic) optimal code and comment to explain code

--- java_space/SwapNodesinPairs.java
+++ java_space/SwapNodesinPairs.java
@@ -10,12 +10,20 @@ public class SwapNodesinPairs{
         
     }
      public ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null) return head;
 
-        ListNode  temp = head.next.next;
+        if(head == null || head.next == null) return head;
+        // create first node for swap two adjacent nodes
         ListNode first = new ListNode(head.next.val);
-        head.next = swapPairs(temp);
+
+        // recursive for the second . next node cause the same action for next.next node from swap location node 
+        head.next = swapPairs(head.next.next);
+        
+        //the current node gonna add two first node;
         first.next = head;
+                
         return first;   
+    }
+    public static void main(String[] args) {
+        
     }
 }
