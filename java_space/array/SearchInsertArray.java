86c24d9cb1e4a1b152980e929fd5ac3b64eef96f
DOXUANHAU <doxuanhau08022003@gmail.com>
2026-03-11 10:17:43 +0700

    refactor(logic) write comment in code for more understand

--- java_space/array/SearchInsertArray.java
+++ java_space/array/SearchInsertArray.java
@@ -5,11 +5,18 @@ public class SearchInsertArray {
         if(nums == null || nums.length == 0) return -1;
         
         int left = 0 , right = nums.length - 1 ;
+        // when the loop end left always at the index target + 1
         while (left <= right) {
             int index = ( right - left ) / 2 + left;
             int value = nums[index];
+
+            // if value at the target => index of target is the same as value
             if(value == target) return index;
+
+
             if(value > target) right = index - 1;
+
+
             if(value < target ) left = index + 1;
         }
         return left;
@@ -17,7 +24,6 @@ public class SearchInsertArray {
     public static void main(String[] args) {
         int[] nums = new int[]{1};
         SearchInsertArray array = new SearchInsertArray();
-
         System.out.println(array.searchInsert(nums, 2));
     }
 }
