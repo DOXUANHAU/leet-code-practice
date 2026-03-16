ec3af5fece4c6bcdac95279db5faab9dad71bcd9
DOXUANHAU <doxuanhau08022003@gmail.com>
2026-03-16 10:51:18 +0700

    refactor(logic) combination 2 implement fron leet code

---
+++ java_space/array/CombinationSumII.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumII {
+   List<List<Integer>> result = new ArrayList<>();
+
+public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    backtrack(candidates, target, 0, new ArrayList<>());
+    return result;
+}
+
+private void backtrack(int[] candidates, int target, int start, List<Integer> path) {
+
+    if (target == 0) {
+        result.add(new ArrayList<>(path));
+        return;
+    }
+
+    for (int i = start; i < candidates.length; i++) {
+
+        if (i > start && candidates[i] == candidates[i - 1]) continue;
+
+        if (candidates[i] > target) break;
+
+        path.add(candidates[i]);
+
+        backtrack(candidates, target - candidates[i], i + 1, path);
+
+        path.remove(path.size() - 1);
+    }
+}
+}
