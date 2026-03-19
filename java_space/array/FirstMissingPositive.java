0c2467788c9768836f59ec66914f1360fe64fe23
DOXUANHAU <doxuanhau08022003@gmail.com>
2026-03-19 21:54:52 +0700

    refactor(logic) permutaions logic using backtracking

--- java_space/array/FirstMissingPositive.java
+++ java_space/array/FirstMissingPositive.java
@@ -1,23 +1,26 @@
 package array;
 public class  FirstMissingPositive {
    public int firstMissingPositive(int[] nums) {
-    int n = nums.length;
 
-    for (int i = 0; i < n; i++) {
-        while (nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
+    for (int i = 0; i < nums.length; i++) {
+        // swap number into the right at index = num[i] - 1
+        // ignore the negative and duplicate number 
+        while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i]-1] != nums[i]) {
             int temp = nums[i];
             nums[i] = nums[temp - 1];
             nums[temp - 1] = temp;
         }
     }
 
-    for (int i = 0; i < n; i++) {
+
+    // search if num[i] does not equal i + 1 ==> missing value 
+    for (int i = 0; i < nums.length; i++) {
         if (nums[i] != i + 1) {
             return i + 1;
         }
     }
 
-    return n + 1;
+    return nums.length + 1;
 }
 public static void main(String[] args) {
     int[] nums = new int[] {-1,5,2,6,8,4,2,5};
