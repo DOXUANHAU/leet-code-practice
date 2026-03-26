bd75b4837357fb2c07f144be57469c160b7e7053
DOXUANHAU <118369683+DOXUANHAU@users.noreply.github.com>
2026-03-26 13:59:50 +0700

    Modify WordSearch to track result with a boolean flag
    Refactor WordSearch to use a result flag for backtracking.

--- java_space/array/WordSearch.java
+++ java_space/array/WordSearch.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public class WordSearch {
-
+public boolean result= false;
      public boolean exist(char[][] board, String word) {
         if( word == null || word.length() == 0) return false;
 
@@ -17,20 +17,22 @@ public class WordSearch {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if(board[i][j] == first){
-                   return  backtracking(board, visited, i, j, word, 0)  ;
+                     if
+                    backtracking(board, visited, i, j, word, 0)  ;
                 }
             }
         }
 
-        return false;
+        return result;
 
     }
 
     // backtracking function 
-    private boolean backtracking(char[][] board , boolean visited[][] , int rowStart , int columnStart , String word ,int currnetCharIndex){
+    private void backtracking(char[][] board , boolean visited[][] , int rowStart , int columnStart , String word ,int currnetCharIndex){
 
         if(currnetCharIndex == word.length() - 1 && board[rowStart][columnStart] == word.charAt(currnetCharIndex) ) {
-            return true;
+             result = true ;
+             return;
         }
 
         int[] dx = {-1,1,0,0};
@@ -64,7 +66,7 @@ public class WordSearch {
 
 
         
-        return false;
+        
     }
     
 public static void main(String[] args) {
